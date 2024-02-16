2b70bfd203888030fd15fac7df4c3f987597b2c3
zeeshan.ahmad <zeeshan.ahmad@fplabs.tech>
2024-02-17 02:30:58 +0530

    sending static html page

--- server.mjs
+++ server.mjs
@@ -1,11 +1,16 @@
+import { readFileSync } from "fs";
 import * as http from "http";
 // creat a server
 const server = http.createServer((req, res) => {
   console.log(req.url);
-  res.write("kch to kahiye ki log kehte hain");
+  // res.write("kch to kahiye ki log kehte hain");
   //we can use switch case for multiple path
   if (req.url === "/get") {
     return res.end(" haal ksa hai janab ka");
+  }
+  if (req.url === "/getsite") {
+    const data = readFileSync("./index.html").toString();
+    return res.end(data);
   }
   res.end(`welcome to end point ${req.url}`);
 });
