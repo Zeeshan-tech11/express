69d270f4c806ca8f31a078512ec80b2e4a21f714
zeeshan.ahmad <zeeshan.ahmad@fplabs.tech>
2024-02-18 11:36:35 +0530

    learn  middleware

--- express.js
+++ express.js
@@ -1,8 +1,29 @@
 const express = require("express");
 
 const server = express();
-
+server.use(
+  (re, rs, next) => {
+    console.log("first");
+    next();
+  },
+  (re, rs, next) => {
+    console.log("sec");
+    next();
+  }
+);
+server.get(
+  "/",
+  (req, res, next) => {
+    console.log("just loggong", req.url);
+    next();
+  },
+  (re, res, next) => {
+    console.log("n nested mw");
+    next();
+  }
+);
 server.get("/", (req, res) => {
+  console.log("in sec middleware");
   res.send("welcome to express");
 });
 server.listen(3000, () => {
